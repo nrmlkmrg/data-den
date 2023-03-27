a4ec60bc0d70cc03561463402c95253af02b327d
Nirmal <127082483+nrmlkmrg@users.noreply.github.com>
2023-03-28 01:48:26 +0530

    Update Covid19-dataexploration.sql

--- Covid19-dataexploration.sql
+++ Covid19-dataexploration.sql
@@ -9,11 +9,11 @@ order by 3,4
 -- Select data to begin with
 
 Select Location,
-	   date,
-	   total_cases,
-	   new_cases,
-	   total_deaths,
-	   population
+       date,
+       total_cases,
+       new_cases,
+       total_deaths,
+       population
 From PortfolioProject..CovidDeaths
 Where continent is not null
 order by 1,2
@@ -24,10 +24,10 @@ order by 1,2
 -- An indicator to know how likely somebody is going to die if they contact Covid
 
 Select Location,
-	   date,
-	   total_cases,
-	   total_deaths,
-	   (CAST(total_deaths AS float)/CAST(total_cases AS float))*100 AS "%Death"
+       date,
+       total_cases,
+       total_deaths,
+       (CAST(total_deaths AS float)/CAST(total_cases AS float))*100 AS "%Death"
 From PortfolioProject..CovidDeaths
 Where continent is not null
 order by 1,2
@@ -38,10 +38,10 @@ order by 1,2
 -- Shows what percentage of population infected with Covid
 
 Select Location,
-	   date,
-	   population,
-	   total_cases,
-	   (total_cases/population)*100 as "%Infected"
+       date,
+       population,
+       total_cases,
+       (total_cases/population)*100 as "%Infected"
 From PortfolioProject..CovidDeaths
 Where location like '%India%' and continent is not null
 order by 1,2
@@ -51,9 +51,9 @@ order by 1,2
 -- Countries with Highest Infection Rate compared to Population
 
 Select Location,
-	   population,
-	   MAX(cast(total_cases as int)) as HighestCount,
-	   MAX(total_cases/population)*100 as "%Infected"
+       population,
+       MAX(cast(total_cases as int)) as HighestCount,
+       MAX(total_cases/population)*100 as "%Infected"
 From PortfolioProject..CovidDeaths
 Where continent is not null
 Group by Location, population
@@ -64,7 +64,7 @@ Order by [%Infected] DESC
 -- Countries with Highest Death Count per Population
 
 Select Location,
-	   MAX(cast(total_deaths as int)) as TotalDeathToll
+       MAX(cast(total_deaths as int)) as TotalDeathToll
 From PortfolioProject..CovidDeaths
 Where continent is not null
 Group by Location
@@ -76,7 +76,7 @@ Order by TotalDeathToll DESC
 -- Contintents with the highest death count per population
 
 Select location,
-	   MAX(cast(total_deaths as int)) as TotalDeathToll
+       MAX(cast(total_deaths as int)) as TotalDeathToll
 From PortfolioProject..CovidDeaths
 Where continent is null
 Group by location
@@ -116,15 +116,15 @@ Order by 1,2
 -- Shows Percentage of Population that has recieved at least one dose of Vaccine
 
 Select ded.continent,
-	   ded.location,
-	   ded.date,
-	   ded.population,
-	   vax.new_vaccinations,
-	   SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
+       ded.location,
+       ded.date,
+       ded.population,
+       vax.new_vaccinations,
+       SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
 From PortfolioProject..CovidDeaths ded
 Join PortfolioProject..CovidVaxx vax
-		On ded.location = vax.location
-		and ded.date = vax.date
+     On ded.location = vax.location
+     and ded.date = vax.date
 Where ded.continent is not null
 Order by 2,3
 
@@ -136,15 +136,15 @@ With PopvsVax (Continent, Location, Date, Population, new_vaccinations, VaxxCoun
 as
 (
 Select ded.continent,
-	   ded.location,
-	   ded.date,
-	   ded.population,
-	   vax.new_vaccinations,
-	   SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
+       ded.location,
+       ded.date,
+       ded.population,
+       vax.new_vaccinations,
+       SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
 From PortfolioProject..CovidDeaths ded
 Join PortfolioProject..CovidVaxx vax
      On ded.location = vax.location
-	 and ded.date = vax.date
+     and ded.date = vax.date
 Where ded.continent is not null
 )
 Select *, (VaxxCounts/Population)*100 as "%Vaxxed"
@@ -168,15 +168,15 @@ VaxxCounts numeric
 
 Insert into #PercentPopulationGotVaccinated
 Select ded.continent,
-	   ded.location,
-	   ded.date,
-	   ded.population,
-	   vax.new_vaccinations,
-	   SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
+       ded.location,
+       ded.date,
+       ded.population,
+       vax.new_vaccinations,
+       SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
 From PortfolioProject..CovidDeaths ded
 Join PortfolioProject..CovidVaxx vax
-	 On ded.location = vax.location
-	 and ded.date = vax.date
+     On ded.location = vax.location
+     and ded.date = vax.date
 
 
 Select *, (VaxxCounts/Population)*100 as "%Vaxxed"
@@ -190,15 +190,15 @@ USE PortfolioProject
 GO
 Create View PercentPopulationGotVaccinated as 
 Select ded.continent,
-	   ded.location,
-	   ded.date,
-	   ded.population,
-	   vax.new_vaccinations,
-	   SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
+       ded.location,
+       ded.date,
+       ded.population,
+       vax.new_vaccinations,
+       SUM(CONVERT(bigint, vax.new_vaccinations)) OVER (Partition by ded.location Order by ded.location, ded.date) as VaxxCounts
 From PortfolioProject..CovidDeaths ded
 Join PortfolioProject..CovidVaxx vax
-	 On ded.location = vax.location
-	 and ded.date = vax.date
+     On ded.location = vax.location
+     and ded.date = vax.date
 Where ded.continent is not null
 
 
